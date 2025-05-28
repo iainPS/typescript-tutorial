a38fcc0ba844fe5a7e7eaeda0b8dfb78a5ca2843
Iain Paterson-Stephens <ips@ipsMcPro4.local>
2025-05-28 22:03:00 +0100

    messing tutorial 2 up

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,35 @@
 var character = 'mario';
 console.log(character);
 var inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) {
-    console.log(input);
-});
+console.log(inputs);
+var toFromInput = inputs.item(0);
+var detailsInput = inputs.item(1);
+var handleDetailsInputEvent = function (event) {
+    var detailsValue = "DETAILS_" + toFromInput.value;
+    detailsInput.value = detailsValue;
+    console.log(toFromInput.value);
+};
+toFromInput.addEventListener("input", handleDetailsInputEvent);
+var adder = function (a, b) {
+    return a + b;
+};
+var minuser = function (a, b) {
+    return a - b * a;
+};
+console.log(adder(1, 2));
+console.log(minuser(1, 2));
+var performSomeMaths = function (a, b, mathsDoer) {
+    return mathsDoer(a, b);
+};
+console.log(performSomeMaths(10, 8, adder));
+console.log(performSomeMaths(10, 8, minuser));
+var performSomeMathsOnPositiveNumbers = function (a, b, mathsDoer) {
+    var positiveA = Math.abs(a);
+    var positiveB = Math.abs(b);
+    return mathsDoer(positiveA, positiveB);
+};
+console.log(performSomeMathsOnPositiveNumbers(-10, -8, adder));
+console.log(performSomeMathsOnPositiveNumbers(-10, -8, minuser));
+console.log(performSomeMathsOnPositiveNumbers(-10, -8, function (a, b) {
+    return a / b;
+}));
--- sandbox.ts
+++ sandbox.ts
@@ -4,6 +4,65 @@ console.log(character);
 
 const inputs = document.querySelectorAll('input');
 
-inputs.forEach(input => {
-  console.log(input);
-});
+console.log(inputs);
+
+const toFromInput  = inputs.item(0)
+const detailsInput = inputs.item(1)
+
+const handleDetailsInputEvent = (event: InputEvent) => {
+     const detailsValue = "DETAILS_" + toFromInput.value;
+     detailsInput.value = detailsValue;
+     console.log(toFromInput.value)
+}
+
+toFromInput.addEventListener(
+     "input",
+    handleDetailsInputEvent
+ )
+
+const adder = (a: number, b: number): number => {
+    return a + b;
+}
+
+const minuser = (a: number, b: number): number => {
+    return a - b * a;
+}
+
+console.log(adder (1, 2))
+console.log(minuser (1, 2))
+
+const performSomeMaths = (
+    a: number,
+    b: number,
+    mathsDoer: (a: number, b: number) => number
+) => {
+    return mathsDoer(a, b)
+}
+
+console.log(performSomeMaths(10, 8, adder))
+console.log(performSomeMaths(10, 8, minuser))
+
+
+const performSomeMathsOnPositiveNumbers = (
+    a: number,
+    b: number,
+    mathsDoer: (a: number, b: number) => number
+) => {
+    let positiveA = Math.abs(a)
+    let positiveB = Math.abs(b)
+    return mathsDoer(positiveA, positiveB)
+}
+
+console.log(performSomeMathsOnPositiveNumbers(-10, -8, adder))
+console.log(performSomeMathsOnPositiveNumbers(-10, -8, minuser))
+
+
+console.log(
+    performSomeMathsOnPositiveNumbers(
+        -10,
+        -8,
+        (a, b) => {
+            return a / b;
+        }
+    )
+)
